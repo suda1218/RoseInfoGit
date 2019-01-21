239be1c3f303e3abf09df9b21d9462797b7f70c9
Chuhan Feng <fengc1@fengc1-5510.rose-hulman.edu>
2019-01-20 21:20:31 -0500

    test

--- app/src/main/java/rose/chenm6/roseinfo/MainActivity.kt
+++ app/src/main/java/rose/chenm6/roseinfo/MainActivity.kt
@@ -10,11 +10,9 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
